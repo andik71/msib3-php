cc84745440b6cf6395b40448d3375da00adf2909
Andika Dwiyanto <95038983+andik71@users.noreply.github.com>
2022-10-02 04:20:59 +0800

    andikaDwiyanto - php4

--- index.php
+++ index.php
@@ -406,7 +406,7 @@ $daftarPegawai = [
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>andikaDwiynato - Class & Object Pegawai</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
