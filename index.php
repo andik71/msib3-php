d5d9476a150d9968458b0c4162bd8f93b8566f43
Andika Dwiyanto <95038983+andik71@users.noreply.github.com>
2022-10-02 03:33:55 +0800

    andikaDwiyanto - php4

---
+++ index.php
@@ -0,0 +1,436 @@
+<?php
+require 'Pegawai.php';
+
+// Array Multidimensi
+
+$daftarPegawai = [
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Kabag',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Begitu Sulit',
+        'jabatan' => 'Asmen',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Lupakan Raihan',
+        'jabatan' => 'Staff',
+        'agama' => 'Kristen',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Apalagi Raihan',
+        'jabatan' => 'Manager',
+        'agama' => 'Konghucu',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Baik',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Buddha',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345736,
+        'nama' => 'Lord Satria',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577552736,
+        'nama' => 'Baginda David',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 124345736,
+        'nama' => 'Azzam Ezekiel',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+    [
+        'nip' => 577345412,
+        'nama' => 'Saitama',
+        'jabatan' => 'Manager',
+        'agama' => 'Islam',
+        'status' => 'Menikah'
+    ],
+]
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container-fluid col-lg-12 bg-light p-4">
+        <h1 class="text-center fs-2 mb-0 my-3"><?= Pegawai::PT ?></h1>
+        <p class="text-center fs-4 mb-5">Menampilkan Data Pegawai Menggunakan Class & Object Class</p>
+
+        <div class="row">
+            <?php
+            // Perulangan
+            foreach ($daftarPegawai as $dpegawai) {
+                $pegawai = new Pegawai($dpegawai['nip'], $dpegawai['nama'], $dpegawai['jabatan'], $dpegawai['agama'], $dpegawai['status']);
+                $pegawai->cetak();
+            }
+            ?>
+        </div><br>
+        <h6 class="text-left p-1 mt-0 my-0 col-lg-3">JUMLAH PEGAWAI: <?= Pegawai::$no ?></h6>
+    </div>
+
+    <p class="text-center text-muted p-5 mt-0 my-0 bg-dark small">@andikaDwiyanto, 2022. All rights reserved</p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+
+</html>
